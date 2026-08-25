715e48706173fed9b0af4b0004e549786a2fe50a
vikkikumar07 <vikkikumar2201@gmail.com>
2026-08-25 11:09:55 +0530

    write a L26 q code

---
+++ Array/Questions/L26.java
@@ -0,0 +1,22 @@
+package DSA.Array.Questions;
+
+public class L26 {
+    public static int removeDuplicates(int[] nums) {
+        int k = 1;
+        for(int i =1 ; i< nums.length; i++) {
+
+            //count unique element
+            if(nums[i] != nums[k-1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+
+        System.out.println(removeDuplicates(nums));
+    }
+    
+}
