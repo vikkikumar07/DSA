3aa15b7034c121b6068771ca5ec2c47a126f50e2
vikkikumar07 <vikkiofficial.in@gmail.com>
2026-08-29 14:13:13 +0530

    write a code in java leetcode 231

--- Maths/L231.java
+++ Maths/L231.java
@@ -1,5 +1,16 @@
 package Maths;
 
 public class L231 {
-    
+    public static boolean isPowerOfTwo(int n) {
+        if(n <= 0) {
+            return false;
+        }
+
+        return (n & (n - 1)) == 0;
+    }
+    public static void main(String[] args) {
+        int n = 64;
+        boolean ans = isPowerOfTwo(n);
+        System.out.println(ans);
+    }
 }
