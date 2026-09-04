8ceeb81c80a208c7b6f60712014dd91de74dadbb
vikkikumar07 <vikkiofficial.in@gmail.com>
2026-09-04 21:15:43 +0530

    L2235 add two intiger

---
+++ Maths/L2235.java
@@ -0,0 +1,14 @@
+package Maths;
+
+public class L2235 {
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+
+    }
+
+    public static void main(String[] args) {
+        int n1 = 72;
+        int n2 = -23;
+        System.out.println(sum(n1, n2));
+    }
+}
