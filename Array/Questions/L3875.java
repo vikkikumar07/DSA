ef03f59503481db9e2b973c37d322376a8cc376a
vikkikumar07 <vikkiofficial.in@gmail.com>
2026-09-06 20:42:10 +0530

    L3875 question

---
+++ Array/Questions/L3875.java
@@ -0,0 +1,12 @@
+package Array.Questions;
+
+public class L3875 {
+    public static boolean uniformArray(int[] nums1) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = { 2, 3 };
+        System.out.println(uniformArray(nums1));
+    }
+}
