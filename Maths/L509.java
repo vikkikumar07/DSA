e688c5410d4ddd8ec3bdcacf18a2e7e4e47b3662
vikkikumar07 <vikkiofficial.in@gmail.com>
2026-09-07 10:58:41 +0530

    add fobinacci serese

---
+++ Maths/L509.java
@@ -0,0 +1,26 @@
+package Maths;
+public class L509 {
+
+    // fibpnacci 
+    // 0, 1, 1, 2, 3, 5, .......
+    
+    public static int fib(int n) {
+        if(n <= 1) {
+            return n;
+        }
+
+        int a = 0;
+        int b = 1;
+
+        for(int i = 2; i <= n; i++) {
+            int c = a+b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+    public static void main(String[] args) {
+        System.out.println(fib(7));
+    }
+    
+}
