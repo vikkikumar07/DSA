9320163870f432273675d052897152672d8822b0
vikkikumar07 <vikkiofficial.in@gmail.com>
2026-09-09 20:50:11 +0530

    L3658 question

---
+++ Maths/L3658.java
@@ -0,0 +1,11 @@
+package Maths;
+
+public class L3658 {
+    public static int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcdOfOddEvenSums(5));
+    }
+}
