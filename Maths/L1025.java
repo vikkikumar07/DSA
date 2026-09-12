296ad51e808da64b4c8b4ef955292cf8634dda26
vikkikumar07 <vikkiofficial.in@gmail.com>
2026-09-12 20:12:39 +0530

    L1025 question divisor game

---
+++ Maths/L1025.java
@@ -0,0 +1,13 @@
+package Maths;
+
+public class L1025 {
+    public static boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(divisorGame(2));
+        System.out.println(divisorGame(3));
+
+    }
+}
